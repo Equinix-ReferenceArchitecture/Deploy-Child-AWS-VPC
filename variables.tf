aa4801955f7c891e5f93359cb07b09c82ab9dde3
vipin2060 <vipinvikraman86@gmail.com>
2025-06-01 20:47:52 +0800

    Create variables.tf
    this is to upload variables file

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "vpc_cidr_block" {
+description = "aws vpc cidr" 
+ type        = string
+}
+
+variable "vpc_tag" {
+description = "tag name for vpc" 
+ type        = string
+}
+
+
+variable "securitygroup_name" {
+description = "tag name for security group" 
+ type        = string
+}
+
+
+variable "subnet_cidr_block" {
+description = "aws subnet cidr" 
+ type        = string
+}
+
+
+variable "subnet_tag" {
+description = "aws subnet name" 
+ type        = string
+}
+
+variable "notifications" {
+  description = "Email addresses" 
+  type        = list(string)
+}
+
+variable "authentication_key" {
+  description = "AWS ID"
+  type        = string
+}
+
+variable "aws_access_key" {
+  type    = string
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = ""
+}
+
+variable "aws_region" {
+  type    = string
+  default = ""
+}
